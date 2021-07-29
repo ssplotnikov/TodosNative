da921111df808fee27b563c1cb18ce3d2dd97261
SerezhaPlotnikov <frymeabeaver@gmail.com>
2021-07-29 16:33:40 +0300

    test

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log(1 + 1)
+//form
+const btn = document.getElementById('btnAdd');
+const input = document.getElementById('todo-input')
+
+btn.addEventListener('click',AddTodo);
+
+function AddTodo() {
+		console.log(input.value);
+		input.value = ''
+}
