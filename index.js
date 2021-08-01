b02716d1945791f6737183b016f6316493b53ddf
SerezhaPlotnikov <frymeabeaver@gmail.com>
2021-08-01 14:42:53 +0300

    test0.2

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ const input = document.getElementById('todo-input')
 btn.addEventListener('click', AddTodo)
 
 function AddTodo() {
-  input.value = ''
-  console.log(input.value)
+  if (input.value) {
+    for (i = 0; i < arr.length; i++) {
+      ul.append((li.innerHTML = input.value))
+    }
+    input.value = ''
+  }
 }
+
+const ul = document.createElement('ul')
+const li = document.createElement('li')
+const contblock = document.querySelector('#content-todo')
+li.innerHTML = 'Test'
+contblock.append(ul)
+
+ul.append(li)
